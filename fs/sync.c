a89850deef92b334ed4ec60e677c93b78377d880
Randolf Kastenmaier <randolf.kastenmaier@googlemail.com>
2012-04-07 16:41:33 +0300

    Disable fsync() (franciscofranco)

--- fs/sync.c
+++ fs/sync.c
@@ -220,12 +220,14 @@ static int do_fsync(unsigned int fd, int datasync)
 
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
-	return do_fsync(fd, 0);
+	//return do_fsync(fd, 0);
+	return 0;
 }
 
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
-	return do_fsync(fd, 1);
+	//return do_fsync(fd, 1);
+	return 0;
 }
 
 /**
@@ -295,10 +297,11 @@ EXPORT_SYMBOL(generic_write_sync);
 SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
 				unsigned int flags)
 {
+	/*
 	int ret;
 	struct file *file;
 	struct address_space *mapping;
-	loff_t endbyte;			/* inclusive */
+	loff_t endbyte;			// inclusive
 	int fput_needed;
 	umode_t i_mode;
 
@@ -317,17 +320,17 @@ SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
 
 	if (sizeof(pgoff_t) == 4) {
 		if (offset >= (0x100000000ULL << PAGE_CACHE_SHIFT)) {
-			/*
-			 * The range starts outside a 32 bit machine's
-			 * pagecache addressing capabilities.  Let it "succeed"
-			 */
+			//
+			// The range starts outside a 32 bit machine's
+			// pagecache addressing capabilities.  Let it "succeed"
+			//
 			ret = 0;
 			goto out;
 		}
 		if (endbyte >= (0x100000000ULL << PAGE_CACHE_SHIFT)) {
-			/*
-			 * Out to EOF
-			 */
+			//
+			// Out to EOF
+			//
 			nbytes = 0;
 		}
 	}
@@ -335,7 +338,7 @@ SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
 	if (nbytes == 0)
 		endbyte = LLONG_MAX;
 	else
-		endbyte--;		/* inclusive */
+		endbyte--;		// inclusive
 
 	ret = -EBADF;
 	file = fget_light(fd, &fput_needed);
@@ -374,6 +377,8 @@ out_put:
 	fput_light(file, fput_needed);
 out:
 	return ret;
+	*/
+	return 0;
 }
 #ifdef CONFIG_HAVE_SYSCALL_WRAPPERS
 asmlinkage long SyS_sync_file_range(long fd, loff_t offset, loff_t nbytes,
