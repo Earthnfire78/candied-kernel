fffca93574009900901bf1628bf90d2574165703
Randolf Kastenmaier <randolf@kubuntu-server.(none)>
2012-04-15 17:10:36 +0200

    Increase readahead value

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1314,8 +1314,8 @@ int write_one_page(struct page *page, int wait);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-#define VM_MAX_READAHEAD	1024	/* kbytes */ //128
-#define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
+#define VM_MAX_READAHEAD	4096	/* kbytes */ //128 //1024
+#define VM_MIN_READAHEAD	32	/* kbytes (includes current page) */ //16
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 			pgoff_t offset, unsigned long nr_to_read);
