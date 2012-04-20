276616d1dc6b126130b5ca6aa90f1b02c984fb5f
Randolf Kastenmaier <randolf.kastenmaier@googlemail.com>
2012-04-20 12:12:05 +0200

    clean up: governors and schedulers

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -346,12 +346,12 @@ extern struct cpufreq_governor cpufreq_gov_performance;
 #endif
 #ifdef CONFIG_CPU_FREQ_DEFAULT_GOV_PERFORMANCE
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_performance)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_POWERSAVE)
-extern struct cpufreq_governor cpufreq_gov_powersave;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_powersave)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_USERSPACE)
-extern struct cpufreq_governor cpufreq_gov_userspace;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_userspace)
+//#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_POWERSAVE)
+//extern struct cpufreq_governor cpufreq_gov_powersave;
+//#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_powersave)
+//#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_USERSPACE)
+//extern struct cpufreq_governor cpufreq_gov_userspace;
+//#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_userspace)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND)
 extern struct cpufreq_governor cpufreq_gov_ondemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemand)
@@ -361,9 +361,9 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
 extern struct cpufreq_governor cpufreq_gov_lionheart;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lionheart)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
-extern struct cpufreq_governor cpufreq_gov_interactive;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+//#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
+//extern struct cpufreq_governor cpufreq_gov_interactive;
+//#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
 extern struct cpufreq_governor cpufreq_gov_interactivex;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactivex)
